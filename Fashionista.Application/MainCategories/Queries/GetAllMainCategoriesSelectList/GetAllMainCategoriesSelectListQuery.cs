53356ce923cbbb64f828f67d1f287af65c7fa245
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2019-11-19 20:03:12 +0200

    Added GetAllMainCategoriesSelectListQuery

---
+++ Fashionista.Application/MainCategories/Queries/GetAllMainCategoriesSelectList/GetAllMainCategoriesSelectListQuery.cs
@@ -0,0 +1,12 @@
+namespace Fashionista.Application.MainCategories.Queries.GetAllMainCategoriesSelectList
+{
+    using System.Collections.Generic;
+
+    using Fashionista.Application.Common.Models;
+    using MediatR;
+
+    public class GetAllMainCategoriesSelectListQuery : IRequest<IEnumerable<MainCategoryLookupModel>>
+    {
+
+    }
+}
---
+++ Fashionista.Application/MainCategories/Queries/GetAllMainCategoriesSelectList/GetAllMainCategoriesSelectListQueryHandler.cs
@@ -0,0 +1,39 @@
+namespace Fashionista.Application.MainCategories.Queries.GetAllMainCategoriesSelectList
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Fashionista.Application.Common.Models;
+    using Fashionista.Application.Interfaces;
+    using Fashionista.Domain.Entities;
+    using MediatR;
+    using Microsoft.EntityFrameworkCore;
+
+    public class GetAllMainCategoriesSelectListQueryHandler :
+        IRequestHandler<GetAllMainCategoriesSelectListQuery, IEnumerable<MainCategoryLookupModel>>
+    {
+        private readonly IDeletableEntityRepository<MainCategory> mainCategoryRepository;
+        private readonly IMapper mapper;
+
+        public GetAllMainCategoriesSelectListQueryHandler(
+            IDeletableEntityRepository<MainCategory> mainCategoryRepository,
+            IMapper mapper)
+        {
+            this.mainCategoryRepository = mainCategoryRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<IEnumerable<MainCategoryLookupModel>> Handle(
+            GetAllMainCategoriesSelectListQuery request,
+            CancellationToken cancellationToken)
+        {
+             return await this.mainCategoryRepository
+                .AllAsNoTracking()
+                .ProjectTo<MainCategoryLookupModel>(this.mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
