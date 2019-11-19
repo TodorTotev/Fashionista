4b0ea4452414c3466caeafe3a2d180ee9482c522
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2019-11-19 12:36:11 +0200

    Administration area

---
+++ Web/Fashionista.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -0,0 +1,10 @@
+﻿namespace Fashionista.Web.Areas.Administration.Controllers
+{
+    using Fashionista.Web.Controllers;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Area("Administration")]
+    public class AdministrationController : BaseController
+    {
+    }
+}
