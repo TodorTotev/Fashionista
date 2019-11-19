0264099463c375cb25e90907892da48e76493ff2
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2019-11-19 22:42:46 +0200

    Added CreateSubCategoryCommandTests

---
+++ Fashionista.Application.Tests/SubCategories/Commands/Create/CreateSubCategoryCommandTests.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Fashionista.Application.Exceptions;
+using Fashionista.Application.Interfaces;
+using Fashionista.Application.MainCategories.Queries.GetAllMainCategoriesSelectList;
+using Fashionista.Application.SubCategories.Commands.Create;
+using Fashionista.Persistence.Repositories;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace Fashionista.Application.Tests.SubCategories.Commands.Create
+{
+    using Fashionista.Application.Tests.Infrastructure;
+    using Fashionista.Domain.Entities;
+
+    public class CreateSubCategoryCommandTests : BaseTest<SubCategory>
+    {
+        [Trait(nameof(SubCategory), "CreateSubCategory command tests")]
+        [Fact(DisplayName = "Request should create SubCategory")]
+        public async Task Handle_ShouldCreateSubCategory()
+        {
+            // Arrange
+            var command = new CreateSubCategoryCommand
+            {
+                Name = "ValidCategory",
+                Description = "ValidDescription",
+                MainCategoryId = 1,
+            };
+
+            var subCategoryRepository = new EfDeletableEntityRepository<SubCategory>(this.dbContext);
+            var test = new CreateSubCategoryCommandHandler(subCategoryRepository, this.mapper);
+
+            // Act
+            var id = await test.Handle(command, It.IsAny<CancellationToken>());
+
+            // Assert
+            var createdCategory = this.deletableEntityRepository
+                .AllAsNoTracking()
+                .SingleOrDefault(x => x.Name == "ValidCategory");
+
+            createdCategory.Name.ShouldBe("ValidCategory");
+            createdCategory.Description.ShouldBe("ValidDescription");
+            createdCategory.MainCategoryId.ShouldBe(1);
+        }
+
+        [Trait(nameof(SubCategory), "CreateSubCategory command tests")]
+        [Fact(DisplayName = "Handle given null request should throw ArgumentNullException")]
+        public async Task Handle_GivenNullRequest_ShouldThrowArgumentNullException()
+        {
+            var sut = new CreateSubCategoryCommandHandler(
+                It.IsAny<IDeletableEntityRepository<SubCategory>>(),
+                It.IsAny<IMapper>());
+
+            await Should.ThrowAsync<ArgumentNullException>(
+                sut.Handle(null, It.IsAny<CancellationToken>()));
+        }
+
+        [Trait(nameof(SubCategory), "CreateSubCategory command tests")]
+        [Fact(DisplayName = "Handle given null request should throw ArgumentNullException")]
+        public async Task Handle_GivenInvalidRequest_ShouldThrowEntityAlreadyExistsException()
+        {
+            // Arrange
+            var command = new CreateSubCategoryCommand
+            {
+                Name = "SubCategory1",
+                Description = "TestDesc",
+                MainCategoryId = 1,
+            };
+
+            var sut = new CreateSubCategoryCommandHandler(this.deletableEntityRepository, this.mapper);
+
+            // Act & Assert
+            await sut.Handle(command, It.IsAny<CancellationToken>());
+            await Should.ThrowAsync<EntityAlreadyExistsException>(sut.Handle(command, It.IsAny<CancellationToken>()));
+        }
+    }
+}
