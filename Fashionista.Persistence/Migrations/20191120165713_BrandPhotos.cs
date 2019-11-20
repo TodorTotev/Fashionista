fb6439266ce8fda3ba5ed5e10a17580ff58a17e3
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2019-11-20 19:05:33 +0200

    Edited Brand Entity

---
+++ Fashionista.Persistence/Migrations/20191120165713_BrandPhotos.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fashionista.Persistence.Migrations
+{
+    public partial class BrandPhotos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photo_Brands_BrandId",
+                table: "Photo");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Photo_BrandId",
+                table: "Photo");
+
+            migrationBuilder.DropColumn(
+                name: "BrandId",
+                table: "Photo");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BrandPhotoUrl",
+                table: "Brands",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BrandPhotoUrl",
+                table: "Brands");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BrandId",
+                table: "Photo",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photo_BrandId",
+                table: "Photo",
+                column: "BrandId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photo_Brands_BrandId",
+                table: "Photo",
+                column: "BrandId",
+                principalTable: "Brands",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
