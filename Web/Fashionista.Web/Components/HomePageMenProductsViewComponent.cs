4c743a3cb55471e6531cb309c873e48710b2fa12
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2019-11-27 16:11:36 +0200

    Added HomePageMenProductsViewComponent

--- Web/Fashionista.Web/Components/HomePageMenProductsViewComponent.cs
+++ Web/Fashionista.Web/Components/HomePageMenProductsViewComponent.cs
@@ -1,7 +1,25 @@
 namespace Fashionista.Web.Components
 {
-    public class HomePageMenProductsViewComponent
+    using System.Threading.Tasks;
+
+    using Fashionista.Application.Products.Queries.GetAllMenProducts;
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomePageMenProductsViewComponent : ViewComponent
     {
-        
+        private readonly IMediator mediator;
+
+        public HomePageMenProductsViewComponent(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = await this.mediator.Send(new GetAllMenProductsQuery());
+
+            return this.View(model);
+        }
     }
 }
