4422e5404e8e2ef99dfdf9e35fc7329ce330ffd0
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2019-11-29 12:26:23 +0200

    Update EditProductQueryHandler.cs

--- Fashionista.Application/Products/Queries/Edit/EditProductQueryHandler.cs
+++ Fashionista.Application/Products/Queries/Edit/EditProductQueryHandler.cs
@@ -10,6 +10,7 @@ namespace Fashionista.Application.Products.Queries.Edit
     using Fashionista.Application.Products.Commands.Edit;
     using Fashionista.Domain.Entities;
     using MediatR;
+    using Microsoft.EntityFrameworkCore;
 
     public class EditProductQueryHandler : IRequestHandler<EditProductQuery, EditProductCommand>
     {
@@ -29,7 +30,8 @@ namespace Fashionista.Application.Products.Queries.Edit
             request = request ?? throw new ArgumentNullException(nameof(request));
 
             var requestedEntity = await this.productRepository
-                                      .GetByIdWithDeletedAsync(request.Id)
+                                      .AllAsNoTracking()
+                                      .SingleOrDefaultAsync(x => x.Id == request.Id, cancellationToken)
                                   ?? throw new NotFoundException(nameof(Product), request.Id);
 
             var command = this.mapper.Map<EditProductCommand>(requestedEntity);
