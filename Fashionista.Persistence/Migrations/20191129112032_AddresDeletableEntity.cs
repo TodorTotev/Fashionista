92604ab0478f208004e9ea4f7c58fb932bf0e73a
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2019-11-29 13:55:24 +0200

    Update Address entity

---
+++ Fashionista.Persistence/Migrations/20191129112032_AddresDeletableEntity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fashionista.Persistence.Migrations
+{
+    public partial class AddresDeletableEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedOn",
+                table: "Addresses",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Addresses",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_IsDeleted",
+                table: "Addresses",
+                column: "IsDeleted");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Addresses_IsDeleted",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedOn",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Addresses");
+        }
+    }
+}
