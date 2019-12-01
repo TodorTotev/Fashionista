a9b1773cb37e09930a47bb6c2b4e71c29598e72f
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2019-12-01 18:34:53 +0200

    Added ShoppingCartController - Index & Add features + layout

---
+++ Web/Fashionista.Web/Controllers/ShoppingCartController.cs
@@ -0,0 +1,66 @@
+namespace Fashionista.Web.Controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Fashionista.Application.Common.Models;
+    using Fashionista.Application.ShoppingCart.Commands.Add;
+    using Fashionista.Application.ShoppingCart.Commands.AddSesssion;
+    using Fashionista.Application.ShoppingCart.Queries.GetAllShoppingCartProducts;
+    using Fashionista.Common;
+    using Fashionista.Infrastructure;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ShoppingCartController : BaseController
+    {
+        public async Task<IActionResult> Index()
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                var model = await this.Mediator.Send(new GetAllShoppingCartProductsQuery());
+
+                if (!model.ShoppingCartProducts.Any())
+                {
+                    return this.Redirect("/");
+                }
+
+                return this.View(model);
+            }
+
+            var test = this.HttpContext.Session;
+
+            var session = this.HttpContext.Session
+                .GetObjectFromJson<List<CartProductLookupModel>>(
+                    GlobalConstants.ShoppingCartKey);
+
+            if (session == null || !session.Any())
+            {
+                return this.Redirect("/");
+            }
+
+            return this.View(new AllShoppingCartProductsViewModel
+            {
+                ShoppingCartProducts = session,
+            });
+        }
+
+        public async Task<IActionResult> Add(AddProductInCartCommand command)
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                await this.Mediator.Send(command);
+                return this.RedirectToAction(nameof(this.Index));
+            }
+
+            var session = await this.Mediator.Send(new AddSessionProductCartCommand { Id = command.Id });
+
+            if (session == null)
+            {
+                return this.Redirect("/");
+            }
+
+            return this.RedirectToAction(nameof(this.Index));
+        }
+    }
+}
