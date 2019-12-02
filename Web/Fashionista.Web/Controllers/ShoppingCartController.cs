d47f5fe44a618ea406f31c385c4ce8609cb14b43
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2019-12-02 17:54:14 +0200

    Added ShoppingCartController - Full functionality + layout

--- Web/Fashionista.Web/Controllers/ShoppingCartController.cs
+++ Web/Fashionista.Web/Controllers/ShoppingCartController.cs
@@ -1,17 +1,19 @@
-using Fashionista.Application.ShoppingCart.Queries.GetAllSessionShoppingCartProducts;
+using Fashionista.Application.ShoppingCart.Commands.Clear;
+using Fashionista.Application.ShoppingCart.Commands.ClearSession;
 
 namespace Fashionista.Web.Controllers
 {
-    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
 
-    using Fashionista.Application.Common.Models;
     using Fashionista.Application.ShoppingCart.Commands.Add;
     using Fashionista.Application.ShoppingCart.Commands.AddSesssion;
+    using Fashionista.Application.ShoppingCart.Commands.Delete;
+    using Fashionista.Application.ShoppingCart.Commands.DeleteSession;
+    using Fashionista.Application.ShoppingCart.Commands.Edit;
+    using Fashionista.Application.ShoppingCart.Commands.EditSession;
+    using Fashionista.Application.ShoppingCart.Queries.GetAllSessionShoppingCartProducts;
     using Fashionista.Application.ShoppingCart.Queries.GetAllShoppingCartProducts;
-    using Fashionista.Common;
-    using Fashionista.Infrastructure;
     using Microsoft.AspNetCore.Mvc;
 
     public class ShoppingCartController : BaseController
@@ -30,16 +32,16 @@ namespace Fashionista.Web.Controllers
                 return this.View(model);
             }
 
-            var session = await this.Mediator.Send(new GetAllSessionShoppingCartProductsQuery());
+            var products = await this.Mediator.Send(new GetAllSessionShoppingCartProductsQuery());
 
-            if (session == null || !session.Any())
+            if (products == null || !products.Any())
             {
                 return this.Redirect("/");
             }
 
             return this.View(new AllShoppingCartProductsViewModel
             {
-                ShoppingCartProducts = session,
+                ShoppingCartProducts = products,
             });
         }
 
@@ -53,12 +55,67 @@ namespace Fashionista.Web.Controllers
 
             var products = await this.Mediator.Send(new GetAllSessionShoppingCartProductsQuery());
 
-            var session = await this.Mediator.Send(new AddSessionProductCartCommand { Id = command.Id, Session = products });
-
-            if (session == null)
+            if (products == null)
             {
                 return this.Redirect("/");
             }
+
+            await this.Mediator.Send(new AddSessionProductCartCommand { Id = command.Id, Session = products });
+
+            return this.RedirectToAction(nameof(this.Index));
+        }
+
+        public async Task<IActionResult> Edit(EditShoppingCartProductCommand command)
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                await this.Mediator.Send(command);
+                return this.RedirectToAction(nameof(this.Index));
+            }
+
+            var products = await this.Mediator.Send(new GetAllSessionShoppingCartProductsQuery());
+
+            if (products == null || !products.Any())
+            {
+                return this.RedirectToAction(nameof(this.Index));
+            }
+
+            await this.Mediator.Send(new EditSessionShoppingCartProductCommand
+                { Id = command.Id, Quantity = command.Quantity, Session = products });
+
+            return this.RedirectToAction(nameof(this.Index));
+        }
+
+        public async Task<IActionResult> Delete(DeleteProductFromCartCommand command)
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                await this.Mediator.Send(command);
+                return this.RedirectToAction(nameof(this.Index));
+            }
+
+            var products = await this.Mediator.Send(new GetAllSessionShoppingCartProductsQuery());
+
+            if (products == null || !products.Any())
+            {
+                return this.RedirectToAction(nameof(this.Index));
+            }
+
+            await this.Mediator.Send(new DeleteSessionProductFromCartCommand { Id = command.Id, Session = products });
+
+            return this.RedirectToAction(nameof(this.Index));
+        }
+
+        public async Task<IActionResult> Clear(ClearShoppingCartCommand command)
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                await this.Mediator.Send(command);
+                return this.RedirectToAction(nameof(this.Index));
+            }
+
+            var products = await this.Mediator.Send(new GetAllSessionShoppingCartProductsQuery());
+            await this.Mediator.Send(new ClearSessionShoppingCartCommand { Session = products });
 
             return this.RedirectToAction(nameof(this.Index));
         }
