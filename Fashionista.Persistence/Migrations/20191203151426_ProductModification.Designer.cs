5ca5e2fd9515c5405c89c96df06db2462a5b5c7a
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2019-12-03 17:14:49 +0200

    Added migration - ProductModification

--- Fashionista.Persistence/Migrations/20191203151426_ProductModification.Designer.cs
+++ Fashionista.Persistence/Migrations/20191203151426_ProductModification.Designer.cs
@@ -4,14 +4,16 @@ using Fashionista.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Fashionista.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191203151426_ProductModification")]
+    partial class ProductModification
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Fashionista.Persistence/Migrations/20191203151426_ProductModification.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fashionista.Persistence.Migrations
+{
+    public partial class ProductModification : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductColors_ProductColorId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductSizes_ProductSizeId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ProductColorId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ProductSizeId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ProductColorId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ProductSizeId",
+                table: "Products");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductColorId",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductSizeId",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductColorId",
+                table: "Products",
+                column: "ProductColorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductSizeId",
+                table: "Products",
+                column: "ProductSizeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductColors_ProductColorId",
+                table: "Products",
+                column: "ProductColorId",
+                principalTable: "ProductColors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductSizes_ProductSizeId",
+                table: "Products",
+                column: "ProductSizeId",
+                principalTable: "ProductSizes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
