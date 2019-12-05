1c4d6a3e6770d9f1fa158aa2b025f737643a7e3c
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2019-12-05 14:32:54 +0200

    Update CreateOrderQueryHandler.cs

--- Fashionista.Application/Orders/Queries/Create/CreateOrderQueryHandler.cs
+++ Fashionista.Application/Orders/Queries/Create/CreateOrderQueryHandler.cs
@@ -1,30 +1,49 @@
-#pragma warning disable 1998
-
 namespace Fashionista.Application.Orders.Queries.Create
 {
     using System;
+    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
 
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Fashionista.Application.Common.Models;
     using Fashionista.Application.Interfaces;
     using Fashionista.Application.Orders.Commands.Create;
+    using Fashionista.Domain.Entities;
     using MediatR;
+    using X.PagedList;
 
     public class CreateOrderQueryHandler : IRequestHandler<CreateOrderQuery, CreateOrderCommand>
     {
         private readonly IUserAssistant userAssistant;
+        private readonly IDeletableEntityRepository<Address> addressesRepository;
+        private readonly IMapper mapper;
 
-        public CreateOrderQueryHandler(IUserAssistant userAssistant)
+        public CreateOrderQueryHandler(
+            IUserAssistant userAssistant,
+            IDeletableEntityRepository<Address> addressesRepository,
+            IMapper mapper)
         {
             this.userAssistant = userAssistant;
+            this.addressesRepository = addressesRepository;
+            this.mapper = mapper;
         }
 
         public async Task<CreateOrderCommand> Handle(CreateOrderQuery request, CancellationToken cancellationToken)
         {
             request = request ?? throw new ArgumentNullException(nameof(request));
+
+            var addresses = await this.addressesRepository
+                .All()
+                .Where(x => x.ApplicationUserId == this.userAssistant.UserId)
+                .ProjectTo<AddressLookupModel>(this.mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
             return new CreateOrderCommand
             {
                 CustomerInformation = this.userAssistant.FullName,
+                Addresses = addresses,
             };
         }
     }
