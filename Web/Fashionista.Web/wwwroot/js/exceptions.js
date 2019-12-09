1b78756092c0eacfa371ea1012883cabfd807cd4
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2019-12-09 16:01:42 +0200

    Added exceptions.js

---
+++ Web/Fashionista.Web/wwwroot/js/exceptions.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/userNotificationHub").build();
+
+function alertFadeFunc() {
+    window.setTimeout(function () {
+        $("#alertDiv").fadeTo(500, 0).slideUp(500, function () {
+            $(this).remove();
+        });
+    }, 7000);
+
+}
+
+connection.on("ReceivePushNotification", function (messageHeader, message, notificationType) {
+    let element = document.getElementById("alertPanel");
+    element.style.display = "block";
+    let alertType = '';
+    switch (notificationType) {
+        case 'Error':
+            alertType = 'alert-danger';
+            break;
+        case 'Warning':
+            alertType = 'alert-warning';
+            break;
+        case 'Info':
+            alertType = 'alert-primary';
+            break;
+        default:
+            alertType = 'alert-success';
+            break;
+    }
+    let html = `<div id="alertDiv" class="alert ${alertType} alert-dismissible fade show" role="alert"><strong>${messageHeader}</strong> ${message}<button type = "button" class="close" data-dismiss="alert" aria-label="Close">        <span aria-hidden="true">&times;</span>  </button ></div>`;
+    element.innerHTML = html;
+
+    alertFadeFunc();
+});
+
+connection.start();
